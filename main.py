b0066346b4cbe94b6222b92b50c453163be648a5
Emmanuel <emmanueltar14@gmail.com>
2023-02-11 20:56:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import tkinter
+from tkinter import *
+from textblob import TextBlob
+
+
+
+root=Tk()
+root.title("Spelling Checker")
+root.geometry("700x400")
+root.configure(background="#dae6f6")
+root.resizable(False,False)
+
+
+#Function for checking the Spelling
+def check_spelling():
+    word=enter_text.get()
+    a=TextBlob(word)
+    right=str(a.correct())
+
+    cs=Label(root,text="Correct Text is :",font=("poppins",20),bg="#dae6f6",fg="#364971")
+    cs.place(x=100,y=250)
+    spell.config(text=right)
+
+
+heading=Label(root,text="Spelling Checker", font=("Trebuchet",30,"bold"),bg="#dae6f6",fg="#364971")
+heading.pack(pady=(50,0))
+
+enter_text=Entry(root,justify="center",width=30,font=("poppins",25),bg="white",border=2)
+enter_text.pack(pady=10)
+enter_text.focus()
+
+button=Button(root,text="Check",font=("arial",20,"bold"),fg="white",bg="red",command=check_spelling)
+button.pack()
+
+spell=Label(root,font=("poppins",20),bg="#dae6f6",fg="#364971")
+spell.place(x=350,y=250)
+
+
+
+
+root.mainloop()
